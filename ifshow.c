df5a966f77d86b9f0cbd046f57f424d5e48bd30f
Mattéo GARCIA <matteolcsgrc@gmail.com>
2026-01-03 16:27:03 +0100

    feat : show one interface

--- ifshow.c
+++ ifshow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <ifaddrs.h>
+#include <string.h>
 #include <netinet/in.h>
 
 void to_string_ipv4(struct sockaddr_in *addr)
@@ -59,3 +60,37 @@ void show_all_interfaces(void)
 
     freeifaddrs(ifaddr);
 }
+
+void show_one_interface(const char *ifname)
+{
+    struct ifaddrs *ifaddr;
+    struct ifaddrs *ifa;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        return;
+    }
+
+    ifa = ifaddr;
+    while (ifa != NULL) {
+
+        if (ifa->ifa_addr != NULL) {
+
+            if (strcmp(ifa->ifa_name, ifname) == 0) {
+
+                if (ifa->ifa_addr->sa_family == AF_INET) {
+                    printf("%s IPv4: ", ifa->ifa_name);
+                    to_string_ipv4((struct sockaddr_in *)ifa->ifa_addr);
+                }
+                else if (ifa->ifa_addr->sa_family == AF_INET6) {
+                    printf("%s IPv6: ", ifa->ifa_name);
+                    to_string_ipv6((struct sockaddr_in6 *)ifa->ifa_addr);
+                }
+            }
+        }
+
+        ifa = ifa->ifa_next;
+    }
+
+    freeifaddrs(ifaddr);
+}
+
