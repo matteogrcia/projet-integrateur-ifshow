eead76653f70971529804245cfaab49f076f342d
Mattéo GARCIA <matteolcsgrc@gmail.com>
2026-01-03 16:35:52 +0100

    feat : prefixe (count bit 1) + main

--- ifshow.c
+++ ifshow.c
@@ -4,6 +4,44 @@
 #include <string.h>
 #include <netinet/in.h>
 
+
+int get_prefix_length(struct sockaddr *netmask)
+{
+    int count = 0;
+    unsigned char *bytes;
+    int i;
+
+    if (netmask->sa_family == AF_INET) {
+
+        bytes = (unsigned char *)
+                &((struct sockaddr_in *)netmask)->sin_addr;
+
+        for (i = 0; i < 4; i++) {
+            unsigned char b = bytes[i];
+            while (b) {
+                count += b & 1;
+                b >>= 1;
+            }
+        }
+    }
+    else if (netmask->sa_family == AF_INET6) {
+
+        bytes = (unsigned char *)
+                &((struct sockaddr_in6 *)netmask)->sin6_addr;
+
+        for (i = 0; i < 16; i++) {
+            unsigned char b = bytes[i];
+            while (b) {
+                count += b & 1;
+                b >>= 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+
 void to_string_ipv4(struct sockaddr_in *addr)
 {
     char ip_string[INET_ADDRSTRLEN];
@@ -94,3 +132,19 @@ void show_one_interface(const char *ifname)
     freeifaddrs(ifaddr);
 }
 
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2 && strcmp(argv[1], "-a") == 0) {
+        show_all_interfaces();
+    }
+    else if (argc == 3 && strcmp(argv[1], "-i") == 0) {
+        show_one_interface(argv[2]);
+    }
+    else {
+        printf("Erreur lors de l'exécution");
+        return 1;
+    }
+
+    return 0;
+}
